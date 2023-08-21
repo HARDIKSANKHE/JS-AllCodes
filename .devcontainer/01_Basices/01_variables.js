7fba96ba84813206ca9c0d9e1864d92a9f0e91b5
HARDIKSANKHE <119942785+HARDIKSANKHE@users.noreply.github.com>
2023-08-21 14:13:07 +0000

    datatypes &variables

---
+++ .devcontainer/01_Basices/01_variables.js
@@ -0,0 +1,10 @@
+const accId = 17
+let accEmail = "Hs@gmail.com"
+var accPass = "12345"
+// We cannot chance the const values 
+// Using the table display methord to display the code
+// Preffer not to use log cuz of issues in block scopeand functional scope
+accCity="Mumbai"
+accEmail="123@gmail.com"
+accPass="1212331"
+console.table([accId,accEmail,accPass])
---
+++ .devcontainer/01_Basices/02_datatypes.js
@@ -0,0 +1,9 @@
+// As we learned in last lecture we cannot change Const valuse once used and also we cannot use var more oftern its not a good praactice so we will use let 
+// Data types
+let name = "Hardik"
+let age = 19
+let islogIn = false
+// Here are some Daata types in JS
+// number, bigint (Big number to be stored here),String [], boolean True or Flase
+// null it is a stand alone value 
+// undefined 
